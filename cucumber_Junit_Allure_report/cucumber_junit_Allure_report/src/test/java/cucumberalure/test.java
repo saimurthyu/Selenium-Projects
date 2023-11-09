c06ea2a05d98fd8b55cdab2909ca227d6e9dcfc3
Sai Murthy Uppada <54973387+saimurthyu@users.noreply.github.com>
2023-11-09 19:08:18 +0530

    Update test.java

--- cucumber_Junit_Allure_report/cucumber_junit_Allure_report/src/test/java/cucumberalure/test.java
+++ cucumber_Junit_Allure_report/cucumber_junit_Allure_report/src/test/java/cucumberalure/test.java
@@ -13,22 +13,22 @@ public class test {
 
 	@When("User enters username as {string}")
 	public void user_enters_username_as(String string) {
-		System.out.println(string+" when vamshi2");
+		System.out.println(string+" when sai2");
 	}
 
 	@And("User enters password as {string}")
 	public void user_enters_password_as(String string) {
-		System.out.println(string+" when and vamshi2");
+		System.out.println(string+" when and sai2");
 	}
 
 	@Then("User should be able to login sucessfully")
 	public void user_should_be_able_to_login_sucessfully() {
-		System.out.println(" then vamshi1");
+		System.out.println(" then sai1");
 	}
 
 	@And("Error message {string} should be displayed")
 	public void error_message_should_be_displayed(String string) {
-	   System.out.println(string+" then vamshi2");
+	   System.out.println(string+" then sai2");
 	}
 
 }
